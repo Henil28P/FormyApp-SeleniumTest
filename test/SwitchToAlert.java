59dd622eb954135a11a8dd36e6661334b4e02f2a
Henil <henilp467@gmail.com>
2024-12-25 23:33:22 +1100

    [SwitchToAlert.java]: automate tests to find and click the alert button and using 'alert()' of 'Alert' class to switch driver's focus to alert box and close it after test

--- test/SwitchToAlert.java
+++ test/SwitchToAlert.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,6 +16,14 @@ public class SwitchToAlert {
 
         // Navigate WebDriver to the switch-window (web page to be tested)
         driver.get("https://formy-project.herokuapp.com/switch-window");
+
+        // Firstly, click the "Open alert" button by using its ID as locator
+        WebElement alertButton = driver.findElement(By.id("alert-button"));
+        alertButton.click(); // Click the above button found on the web page
+
+        // Use the Alert class to define a new alert object and theen switch to that alert
+        Alert alert = driver.switchTo().alert(); // to switch the driver's focus to the above opened alert box
+        alert.accept(); // to press "Ok" on the alert box and close it
 
         // Quit the driver instance
         driver.quit();
