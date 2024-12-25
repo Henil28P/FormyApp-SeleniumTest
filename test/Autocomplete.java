bb367eabec3a7bfd907e211d2c83c9f32ed2828a
Henil <henilp467@gmail.com>
2024-12-25 19:56:34 +1100

    [Autocomplete.java]: automate the autocomplete address field test with dropdown click and text input

--- test/Autocomplete.java
+++ test/Autocomplete.java
@@ -6,12 +6,24 @@ import org.openqa.selenium.By;
 public class Autocomplete {
     public static void main(String[] args) {
 
+        // Set the location for the ChromeDriver
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
+        // Create new instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
 
+        // Navigate WebDriver to the KeyPress (web page to be tested)
         driver.get("https://formy-project.herokuapp.com/autocomplete");
 
+        // Automate the autocomplete field
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("1555 Park Blvd, Palo Alto, CA"); // Send text input to the autocomplete field
+
+        // Find that element (the result that pops up produced by the autocomplete field)
+        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocompleteResult.click(); // Click the result in the dropdown menu
+
+        // Quit the driver instance
         driver.quit();
     }
 }
