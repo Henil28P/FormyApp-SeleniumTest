c035e5e37ed537dcc3d1b729c75384f69b4d16d0
Henil <henilp467@gmail.com>
2024-12-25 19:30:36 +1100

    [KeyboardAndMouseInput]: add helper comments and automate the 'name' field and 'button' click to submit the form

--- test/KeyboardAndMouseInput.java
+++ test/KeyboardAndMouseInput.java
@@ -6,12 +6,25 @@ import org.openqa.selenium.By;
 public class KeyboardAndMouseInput {
     public static void main(String[] args) {
 
+        // Set the location for the ChromeDriver
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
+        // Create new instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
 
+        // Navigate WebDriver to the KeyPress (web page to be tested)
         driver.get("https://formy-project.herokuapp.com/keypress");
 
+        // Automate the 'name' field
+        WebElement name = driver.findElement(By.id("name"));
+        name.click(); // Make the field active or selected (use mouse input to click on the field before entering the name)
+        name.sendKeys("Henil Patel"); // Automate entering a sample name in the name field
+
+        // Automate the "Button" click to submit the form
+        WebElement button = driver.findElement(By.id("button"));
+        button.click();
+
+        // Quit the driver instance
         driver.quit();
     }
 }
