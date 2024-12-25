e51e274c7d94d1dc0f1c3291aaf9cb0fd3524a52
Henil <henilp467@gmail.com>
2024-12-25 19:58:23 +1100

    [Autocomplete.java]: add sleep statement for driver to wait for 1 sec before clicking dropdown menu address result

--- test/Autocomplete.java
+++ test/Autocomplete.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
 public class Autocomplete {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // Set the location for the ChromeDriver
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
@@ -18,6 +18,9 @@ public class Autocomplete {
         // Automate the autocomplete field
         WebElement autocomplete = driver.findElement(By.id("autocomplete"));
         autocomplete.sendKeys("1555 Park Blvd, Palo Alto, CA"); // Send text input to the autocomplete field
+
+        // To avoid NoSuchElementException (timing issue - slow the test down after entering the address
+        Thread.sleep(1000); // sleep for 1000 milliseconds (1 second) to wait for the next element to load from dropdown results
 
         // Find that element (the result that pops up produced by the autocomplete field)
         WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
