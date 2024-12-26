55a4d033e21460286849277dd15cd28013328695
Henil <henilp467@gmail.com>
2024-12-26 22:07:33 +1100

    [Radiobuttons.java]: automate tests for finding radio buttons elements on Formy web page using 3 different methods (id, attribute, xpath)

--- test/Radiobuttons.java
+++ test/Radiobuttons.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 
@@ -11,6 +13,19 @@ public class Radiobuttons {
 
         // Create new instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
+
+        // 1. Find the 1st radio button using by normal id method
+        WebElement radioButton1 = driver.findElement(By.id("radio-button-1"));
+        radioButton1.click(); // Click on the above 1st radio button found
+
+        // 2. Find the 2nd radio button by attribute method (ie. input[attribute='value'])
+        WebElement radioButton2 = driver.findElement(By.id("input[value='option2']"));
+        radioButton2.click(); // Click on the above 2nd radio button found
+
+        // 3. Find the 3rd radio button by XPath method (right-click on radio button element and
+        // click "Inspect" from menu and again right-click on its code and --> "Copy" --> "Copy XPath"
+        WebElement radioButton3 = driver.findElement(By.xpath("/html/body/div[3]/input")); // This XPath shows is the path of all the tag names used to get to that element "radio-button-3"
+        radioButton3.click(); // Click on the above 1st radio button found
 
         // Navigate WebDriver to the radiobutton (web page to be tested)
         driver.get("https://formy-project.herokuapp.com/radiobutton");
